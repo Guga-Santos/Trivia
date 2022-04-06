24ba9f5360f0e5581d9c7932b3a9ae51a7614dc8
GugaSantos <guga.santos.guga@gmail.com>
2022-04-06 18:34:19 -0300

    Requisito 17 ok

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,36 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
-  render() {
-    return (
-      <h1
-        data-testid="ranking-title"
-      >
-        Ranking
-      </h1>
-    );
-  }
+    handlePlayAgain = () => {
+      const { history } = this.props;
+      history.push('/');
+    }
+
+    render() {
+      return (
+        <div>
+          <h1
+            data-testid="ranking-title"
+          >
+            Ranking
+          </h1>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.handlePlayAgain }
+          >
+            Jogue Novamente!
+          </button>
+        </div>
+      );
+    }
 }
+
+Ranking.propTypes = ({
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+});
 
 export default Ranking;
